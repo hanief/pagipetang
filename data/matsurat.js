ed288d19307a491ffd3a3ed018349de677a0cc9a
Hanief <hanief@gmail.com>
2025-08-22 16:36:24 +0700

    update data

--- data/matsurat.js
+++ data/matsurat.js
@@ -5,14 +5,20 @@ export const matsuratData = [
     arabic: "أَعُوذُ بِاللَّهِ السَّمِيعِ الْعَلِيمِ مِنَ الشَّيْطَانِ الرَّجِيمِ",
     translation: "Aku berlindung kepada Allah Yang Maha Mendengar lagi Maha Mengetahui dari godaan syaitan yang terkutuk",
     transliteration: "A'ūdzu billāhi as-samī' al-'alīm min ash-shayṭānir-rajīm",
+    morning: true,
+    evening: true,
+    lite: true,
     repeat: 1
   },
   {
     id: 2,
     name: "Al-Fatihah",
-    arabic: "بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ ١ ٱلۡحَمۡدُ لِلَّهِ رَبِّ ٱلۡعَٰلَمِينَ ٢ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ ٣ مَٰلِكِ يَوۡمِ ٱلدِّينِ ٤ إِيَّاكَ نَعۡبُدُ وَإِيَّاكَ نَسۡتَعِينُ ٥ ٱهۡدِنَا ٱلصِّرَٰطَ ٱلۡمُسۡتَقِيمَ ٦ صِرَٰطَ ٱلَّذِينَ أَنۡعَمۡتَ عَلَيۡهِمۡ غَيۡرِ ٱلۡمَغۡضُوبِ عَلَيۡهِمۡ وَلَا ٱلضَّآلِّينَ ٧",
+    arabic: "بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ ۝١۝ ٱلۡحَمۡدُ لِلَّهِ رَبِّ ٱلۡعَٰلَمِينَ ۝ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ ۝ مَٰلِكِ يَوۡمِ ٱلدِّينِ ۝ إِيَّاكَ نَعۡبُدُ وَإِيَّاكَ نَسۡتَعِينُ ۝ ٱهۡدِنَا ٱلصِّرَٰطَ ٱلۡمُسۡتَقِيمَ ۝ صِرَٰطَ ٱلَّذِينَ أَنۡعَمۡتَ عَلَيۡهِمۡ غَيۡرِ ٱلۡمَغۡضُوبِ عَلَيۡهِمۡ وَلَا ٱلضَّآلِّينَ ۝",
     translation: "Dengan nama Allah Yang Maha Pengasih, Maha Penyayang. Segala puji bagi Allah, Tuhan semesta alam. Yang Maha Pengasih, Maha Penyayang. Yang Menguasai di Hari Pembalasan. Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami meminta pertolongan. Tunjukilah kami jalan yang lurus. Yaitu jalan orang-orang yang telah Engkau beri nikmat kepada mereka; bukan (jalan) mereka yang dimurkai dan bukan (pula jalan) mereka yang sesat",
     transliteration: "Bismi llāhi ar-raḥmāni ar-raḥīm. Al-ḥamdu lillāhi rabbi al-'ālamīn. Ar-raḥmāni ar-raḥīm. Māliki yawmi ad-dīn. Iyyāka na'budu wa iyyāka nasta'īn. Ihdinā aṣ-ṣirāṭa al-mustaqīm. Ṣirāṭa alladhīna an'amta 'alayhim ghayri al-maghḍūbi 'alayhim wa laḍ-ḍāllīn",
+    morning: true,
+    evening: true,
+    lite: true,
     repeat: 1
   },
   {
@@ -21,6 +27,9 @@ export const matsuratData = [
     arabic: "الٓمٓ ١ ذَٰلِكَ ٱلۡكِتَٰبُ لَا رَيۡبَۛ فِيهِۛ هُدٗى لِّلۡمُتَّقِينَ ٢ ٱلَّذِينَ يُؤۡمِنُونَ بِٱلۡغَيۡبِ وَيُقِيمُونَ ٱلصَّلَوٰةَ وَمِمَّا رَزَقۡنَٰهُمۡ يُنفِقُونَ ۙ ٣ وَٱلَّذِينَ يُؤۡمِنُونَ بِمَآ أُنزِلَ إِلَيۡكَ وَمَآ أُنزِلَ مِن قَبۡلِكَ وَبِٱلۡأٓخِرَةِ هُمۡ يُوقِنُونَ ٤ أُوْلَٰٓئِكَ عَلَىٰ هُدٗى مِّن رَّبِّهِمۡۖ وَأُوْلَٰٓئِكَ هُمُ ٱلۡمُفۡلِحُونَ ٥",
     translation: "Alif lam mim. Kitab (Al-Qur'an) ini tidak ada keraguan padanya; petunjuk bagi mereka yang bertakwa. Yaitu orang-orang yang beriman kepada yang ghaib, mendirikan shalat, dan menafkahkan sebagian rezeki yang Kami berikan kepada mereka. Dan orang-orang yang beriman kepada Al-Qur'an dan kitab-kitab yang terdahulu, mereka yakin akan adanya hari akhir. Mereka itu adalah orang-orang yang mendapat petunjuk dari Tuhannya, dan mereka itulah orang-orang yang beruntung",
     transliteration: "Alif lām mīm. Dhālika al-kitābu lā rayba fīh hudan lil-muttaqīn. Alladhīna yu'minūna bil-ghaybi wa yuqīmūna aṣ-ṣalāta wa mimmā razaqnāhum yunfiqūn. Wa alladhīna yu'minūna bimā unzila ilayka wa mā unzila min qablika wa bil-ākhirati hum yūqinūn. Ulā'ika 'alā hudan min rabbihim wa ulā'ika humul-mufliḥūn",
+    morning: true,
+    evening: true,
+    lite: true,
     repeat: 1
   },
   {
@@ -29,6 +38,9 @@ export const matsuratData = [
     arabic: "ٱللَّهُ لَآ إِلَـٰهَ إِلَّا هُوَ ٱلْحَىُّ ٱلْقَيُّومُ ۚ لَا تَأْخُذُهُۥ سِنَةٌۭ وَلَا نَوْمٌۭ ۚ لَّهُۥ مَا فِى ٱلسَّمَـٰوَٰتِ وَمَا فِى ٱلْأَرْضِ ۗ مَن ذَا ٱلَّذِى يَشْفَعُ عِندَهُۥٓ إِلَّا بِإِذْنِهِۦ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَىْءٍۢ مِّنْ عِلْمِهِۦٓ إِلَّا بِمَا شَآءَ ۚ وَسِعَ كُرْسِيُّهُ ٱلسَّمَـٰوَٰتِ وَٱلْأَرْضَ ۖ وَلَا يَـُٔودُهُۥ حِفْظُhُمَا ۚ وَهُوَ ٱلْعَلِىُّ ٱلْعَظِيمُ ٢٥٥ لَآ إِكْرَاهَ فِى ٱلدِّينِ ۖ قَد تَّبَيَّنَ ٱلرُّشْدُ مِنَ ٱلۡغَىِّ ۚ فَمَن يَكْفُرْ بِٱلطَّـٰغُوتِ وَيُؤْمِنۢ بِٱللَّهِ فَقَدِ ٱسْتَمْسَكَ بِٱلۡعُرْوَةِ ٱلۡوُثۡقَىٰ لَا ٱنفِصَامَ لَهَا ۗ وَٱللَّهُ سَمِيعٌ عَلِيمٌ ٢٥٦ ٱللَّهُ وَلِىُّ ٱلَّذِينَ ءَامَنُوا۟ يُخْرِجُهُم مِّنَ ٱلظُّلُمَـٰتِ إِلَى ٱلنُّورِ ۖ وَٱلَّذِينَ كَفَرُوٓا۟ أَوْلِيَآؤُهُمُ ٱلطَّـٰغُوتُ يُخْرِجُونَهُم mِّنَ ٱلنُّورِ إِلَى ٱلظُّلُمَـٰتِ ۗ أُو۟لَـٰٓئِكَ أَصْحَـٰبُ ٱلنَّارِ ۖ هُمْ فِيهَا خَـٰلِدُونَ ٢٥٧",
     translation: "Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafaat di sisi Allah tanpa izin-Nya. Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar. Tidak ada paksaan untuk (memasuki) agama (Islam); sesungguhnya telah jelas jalan yang benar daripada yang sesat. Karena itu barangsiapa yang ingkar kepada Thaghut dan beriman kepada Allah, maka sesungguhnya ia telah berpegang kepada tali yang amat kokoh yang tidak akan putus. Dan Allah Maha Mendengar lagi Maha Mengetahui. Allah Pelindung orang-orang yang beriman; Dia mengeluarkan mereka dari kegelapan (kekafiran) kepada cahaya (iman). Dan orang-orang yang kafir, pelindung-pelindungnya ialah setan, yang mengeluarkan mereka dari cahaya kepada kegelapan (kekafiran). Mereka itu adalah penghuni neraka; mereka kekal di dalamnya",
     transliteration: "Allāhu lā ilāha illā huwal-ḥayyu al-qayyūm lā ta'khudhuhū sinatun walā nawm, lahu mā fī as-samāwāti wa mā fī al-arḍ, man dhā alladhī yashfa'u 'indahu illā bi-idhnih ya'lamu mā bayna aydīhim wa mā khalfahum walā yuḥīṭūna bishay'in min 'ilmihī illā bimā shā', wasi'a kursiyyuhus-samāwāti wal-arḍ, walā ya'ūduhū ḥifẓuhumā, wahuwal-'aliyyul-'aẓīm. Lā ikrāha fī ad-dīn qad tabayyana ar-rushdu minal-ghayyi faman yakfur biṭ-ṭāghūti wa yu'min billāhi faqad istamsaka bil-'urwati al-wuthqā lā infiṣāma lahā, wal-lāhu samī'un 'alīm. Allāhu waliyyu alladhīna āmanū yukhrijuhum mina aẓ-ẓulumāti ilan-nūr, walladhīna kafarū awliyāuhumu aṭ-ṭāghūtu yukhrijūnahum mina an-nūri ilā aẓ-ẓulumāt, ulā'ika aṣḥābu an-nār, hum fīhā khālidūn",
+    morning: true,
+    evening: true,
+    lite: true, 
     repeat: 1
   },
   {
@@ -37,6 +49,20 @@ export const matsuratData = [
     arabic: "لِّلَّهِ مَا فِى ٱلسَّمَـٰوَٰتِ وَمَا فِى ٱلْأَرْضِ ۗ وَإِن تُبْدُوا۟ مَا فِىٓ أَنفُسِكُمْ أَوْ تُخْفُوهُ يُحَاسِبْكُم بِهِ ٱللَّهُ ۖ فَيَغْفِرُ لِمَن يَشَآءُ وَيُعَذِّبُ مَن يَشَآءُ ۗ وَٱللَّهُ عَلَىٰ كُلِّ شَىْءٍۢ قَدِيرٌ ٢٨٤ ءَامَنَ ٱلرَّسُولُ بِمَآ أُنزِلَ إِلَيْهِ مِن رَّبِّهِۦ وَٱلْمُؤْمِنُونَ ۚ كُلٌّ ءَامَنَ بِٱللَّهِ وَمَلَـٰٓئِكَتِهِۦ وَكُتُبِهِۦ وَرُسُلِهِۦ لَا نُفَرِّقُ بَيْنَ أَحَدٍۢ مِّن رُّسُلِهِۦ ۚ وَقَالُوا۟ سَمِعْنَا وَأَطَعْنَا ۖ غُفْرَانَكَ رَبَّنَا وَإِلَيْكَ ٱلْمَصِيرُ ٢٨٥ لَا يُكَلِّفُ ٱللَّهُ نَفْسًا إِلَّا وُسْعَهَا ۚ لَهَا مَا كَسَبَتْ وَعَلَيْهَا مَا ٱكْتَسَبَتْ ۗ رَبَّنَا لَا تُؤَاخِذْنَآ إِن نَّسِينَآ أَوْ أَخْطَأْنَا ۚ رَبَّنَا وَلَا تَحْمِلْ عَلَيْنَآ إِصْرًۭا كَمَا حَمَلْتَهُۥ عَلَى ٱلَّذِينَ مِن قَبْلِنَا ۚ رَبَّنَا وَلَا تُحَمِّلْنَا مَا لَا طَاقَةَ لَنَا بِهِۦ ۖ وَٱعْفُ عَنَّا وَٱغْفِرْ لَنَا وَٱرْحَمْنَآ ۚ أَنتَ مَوْلَىٰنَا فَٱنصُرْنَا عَلَى ٱلْقَوْمِ ٱلْكَـٰفِرِينَ ٢٨٦",
     translation: "Kepunyaan Allah-lah segala apa yang ada di langit dan apa yang ada di bumi. Dan jika kamu melahirkan apa yang ada di dalam hatimu atau kamu menyembunyikannya, niscaya Allah akan membuat perhitungan dengan kamu tentang perbuatanmu itu. Maka Allah mengampuni siapa yang dikehendaki-Nya dan menyiksa siapa yang dikehendaki-Nya; dan Allah Maha Kuasa atas segala sesuatu. Rasul telah beriman kepada Al Qur'an yang diturunkan kepadanya dari Tuhannya, demikian pula orang-orang yang beriman. Semuanya beriman kepada Allah, malaikat-malaikat-Nya, kitab-kitab-Nya dan rasul-rasul-Nya. (Mereka mengatakan): 'Kami tidak membeda-bedakan antara seseorangpun (dengan yang lain) dari rasul-rasul-Nya', dan mereka mengatakan: 'Kami dengar dan kami taat'. (Mereka berdoa): 'Ampunilah kami ya Tuhan kami dan kepada Engkaulah tempat kembali'. Allah tidak membebani seseorang melainkan sesuai dengan kesanggupannya. Ia mendapat pahala (dari kebajikan) yang diusahakannya dan ia mendapat siksa (dari kejahatan) yang dikerjakannya. (Mereka berdoa): 'Ya Tuhan kami, janganlah Engkau hukum kami jika kami lupa atau kami tersalah. Ya Tuhan kami, janganlah Engkau bebankan kepada kami beban yang berat sebagaimana Engkau bebankan kepada orang-orang sebelum kami. Ya Tuhan kami, janganlah Engkau pikulkan kepada kami apa yang tak sanggup kami memikulnya. Beri ma'aflah kami; ampunilah kami; dan rahmatilah kami. Engkaulah Penolong kami, maka tolonglah kami terhadap kaum yang kafir'",
     transliteration: "Lillāhi mā fī as-samāwāti wa mā fī al-arḍi, wa in tubdū mā fī anfusikum aw tukhfūhu yuḥāsibkum bihillāh, fa-yaghfiru liman yashā'u wa yu'adhdhibu man yashā'u, wa-Allāhu 'alā kulli shay'in qadīr. Aamana ar rusulu bimaa unzila ilaihi mir rabbihii wal mu'minuun kullun aamana billahi wa mala-ikatihi wa kutubihi wa rusulihi laa nufarriqu baina ahadim mir rusulihii wa qalu sami'na wa atho'na ghufranaka rabbana wa ilaikal masiir. Laa yukalliful laahu nafsan illaa wus'ahaa; lahaa maa kasabat wa 'alaihaa mak tasabat; Rabbanaa laa tu'aakhiznaaa in naseenaaa aw akhtaanaa; Rabbanaa wa laa tahmil-'alainaaa isran kamaa hamaltahoo 'alal-lazeena min qablinaa; Rabbanaa wa laa tuhammilnaa maa laa taaqata lanaa bih; wa'fu 'annaa waghfir lanaa warhamnaa; Anta mawlaanaa fansurnaa 'alal qawmil kaafireen",
+    morning: true,
+    evening: true,
+    lite: true,
+    repeat: 1
+  },
+  {
+    id: 6,
+    name: "Ali-Imran 1-2",
+    arabic: "الٓمٓ ٱللَّهُ لَآ إِلَٰهَ إِلَّا هُوَ ٱلۡحَيُّ ٱلۡقَيُّومُ",
+    translation: "Alif lam mim. Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia. Yang hidup kekal lagi terus menerus mengurus makhluk-Nya.",
+    transliteration: "Alif lām mīm. Allāhu lā ilāha illā huwal-ḥayyul-qayyụm",
+    morning: true,
+    evening: true,
+    lite: false,
     repeat: 1
   },
   {
@@ -45,6 +71,9 @@ export const matsuratData = [
     arabic: "بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ قُلْ هُوَ ٱللَّهُ أَحَدٌ ١ ٱللَّهُ ٱلصَّمَدُ ٢ لَمْ يَلِدْ وَلَمْ يُولَدْ ٣ وَلَمْ يَكُن لَّهُۥ كُفُوًا أَحَدٌۢ ٤",
     translation: "Dengan nama Allah Yang Maha Pengasih, Maha Penyayang. Katakanlah: 'Dialah Allah, Yang Maha Esa. Allah adalah Tuhan yang bergantung kepada-Nya segala sesuatu. Dia tiada beranak dan tidak pula diperanakkan, dan tidak ada seorangpun yang setara dengan Dia'",
     transliteration: "Bismillāhir-raḥmānir-raḥīm. Qul huwallāhu aḥad. Allāhuṣ-ṣamad. Lam yalid wa lam yūlad. Wa lam yakul-lahū kufuwan aḥad",
+    morning: true,
+    evening: true,
+    lite: true,
     repeat: 3
   },
   {
@@ -53,6 +82,9 @@ export const matsuratData = [
     arabic: "بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ قُلْ أَعُوذُ بِرَبِّ ٱلْفَلَقِ ١ مِن شَرِّ مَا خَلَقَ ٢ وَمِن شَرِّ غَاسِقٍ إِذَا وَقَبَ ٣ وَمِن شَرِّ ٱلنَّفَّـٰثَـٰتِ فِى ٱلْعُقَدِ ٤ وَمِن شَرِّ حَاسِدٍ إِذَا حَسَدَ ٥",
     translation: "Dengan nama Allah Yang Maha Pengasih, Maha Penyayang. Katakanlah: 'Aku berlindung kepada Tuhan Yang Menguasai subuh, dari kejahatan makhluk-Nya, dan dari kejahatan malam apabila telah gelap gulita, dan dari kejahatan wanita-wanita tukang sihir yang menghembus pada buhul-buhul, dan dari kejahatan pendengki bila ia dengki'",
     transliteration: "Bismillāhir-raḥmānir-raḥīm. Qul a'ūdhu birabbil-falaq. Min sharri mā khalaq. Wa min sharri ghāsiqin idhā waqab. Wa min sharrin-naffāthāti fil-'uqad. Wa min sharri ḥāsidin idhā ḥasad",
+    morning: true,
+    evening: true,
+    lite: true,
     repeat: 3
   },
   {
@@ -61,38 +93,53 @@ export const matsuratData = [
     arabic: "بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ قُلْ أَعُوذُ بِرَبِّ ٱلنَّاسِ ١ مَلِكِ ٱلنَّاسِ ٢ إِلَـٰهِ ٱلنَّاسِ ٣ مِن شَرِّ ٱلْوَسْوَاسِ ٱلْخَنَّاسِ ٤ ٱلَّذِى يُوَسْوِسُ فِى صُdُورِ ٱلنَّاسِ ٥ مِنَ ٱلْجِنَّةِ وَٱلنَّاسِ ٦",
     translation: "Dengan nama Allah Yang Maha Pengasih, Maha Penyayang. Katakanlah: 'Aku berlindung kepada Tuhan (yang memelihara dan menguasai) manusia. Raja manusia. Sembahan manusia. Dari kejahatan (bisikan) syaitan yang biasa bersembunyi, yang membisikkan (kejahatan) ke dalam dada manusia, dari (golongan) jin dan manusia'",
     transliteration: "Bismillāhir-raḥmānir-raḥīm. Qul a'ūdhu birabbin-nās. Malikin-nās. Ilāhin-nās. Min sharril-waswāsil-khannās. Alladhī yuwaswisu fī ṣudūrin-nās. Minal-jinnati wan-nās",
+    morning: true,
+    evening: true,
+    lite: true,
     repeat: 3
   },
   {
     id: 9,
-    name: "Tasbih",
-    arabic: "سُبْحَانَ اللَّهِ وَبِحَمْدِهِ عَدَدَ خَلْقِهِ وَرِضَا نَفْسِهِ وَزِنَةَ عَرْشِهِ وَمِدَادَ كَلِمَاتِهِ",
-    translation: "Maha Suci Allah, aku memuji-Nya sebanyak bilangan makhluk-Nya, sesuai dengan keridhaan-Nya, seberat timbangan arsy-Nya, dan sebanyak tinta kalimat-Nya.",
-    transliteration: "Subḥānallāhi wa biḥamdihī 'adada khalqihī wa riḍā nafsihī wa zinata 'arshihī wa midāda kalimātih.",
+    name: "Dzikir pagi",
+    arabic: "أَصْبَحْنَا وَأَصْبَحَ الْمُلْكُ لِلّهَِ وَلْحَمْدُ لِلّهَِ لاَ شَرِيكَ لَهُ لَا إِلَهَ إِلَّاهُوَ وَإِلَيْهِ النُّشُوْرُ",
+    translation: "Kami berpagi hari dan berpagi hari pula kerjaan milik Allah. Segala puji bagi Allah, tiada sekutu bagi-Nya, tiada Tuhan melainkan Dia dan kepada-Nya tempat kembali.",
+    transliteration: "Ash-baḥnā wa ash-baḥal-mulku lillāhi wal-ḥamdulillāhi lā syarīka lahū lā ilāha illāhuwa wa ilaihi an-nusyūr",
+    morning: true,
+    evening: false,
+    lite: true,
     repeat: 3
   },
   {
     id: 10,
-    name: "Doa",
-    arabic: "اللَّهُمَّ إِنِّي أَسْأَلُكَ عِلْمًا نَافِعًا وَرِزْقًا طَيِّبًا وَعَمَلًا مُتَقَبَّلًا",
-    translation: "Ya Allah, sesungguhnya aku memohon kepada-Mu ilmu yang bermanfaat, rezeki yang baik dan amal yang diterima.",
-    transliteration: "Allāhumma innī as'aluka 'ilman nāfi'an wa rizqan ṭayyiban wa 'amalan mutaqabbalan.",
+    name: "Dzikir pagi",
+    arabic: "أَصْبَحْنَا عَلَى فِطْرَةِ اْلإِسْلاَمِ وَعَلَى كَلِمَةِ اْلإِخْلاَصِ وَعَلَى دِيْنِ نَبِيِّنَا مُحَمَّدٍ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ وَعَلَى مِلَّةِ أَبِيْنَا إِبْرَاهِيْمَ حَنِيْفًا مُسْلِمًا وَمَا كَانَ مِنَ الْمُشْرِكِيْنَ",
+    translation: "Kami berpagi hari dalam keadaan fitrah Islam, dalam kalimat ikhlas, dalam agama Nabi kami Muhammad shallallahu 'alaihi wa sallam, dalam agama ayah kami Ibrahim yang lurus, seorang muslim, dan tidaklah termasuk orang-orang musyrik.",
+    transliteration: "Ash-baḥnā 'alā fiṭratil-islāmi wa 'alā kalimatil-ikhlas wa 'alā dīni nabiyyinā muḥammadin ṣallallāhu 'alaihi wa sallam wa 'alā millati abīnā ibrahīma ḥanīfan musliman wa mā kāna minal-musyrikīn.",
+    morning: true,
+    evening: false,
+    lite: true,
     repeat: 1
   },
   {
     id: 11,
-    name: "Tahlil",
-    arabic: "لَا إِلَهَ إِلَّا اللَّهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرٌ",
-    translation: "Tidak ada Tuhan selain Allah, Yang Maha Esa, tidak ada sekutu bagi-Nya. Bagi-Nya kerajaan dan bagi-Nya pujian. Dan Dia Maha Kuasa atas segala sesuatu",
-    transliteration: "La ilaha illallahu wahdahu la sharika lahu, lahul-mulku wa lahul-hamdu wa huwa 'ala kulli shay'in qadir",
+    name: "Dzikir pagi",
+    arabic: "اللَّهُمَّ إِنِّي أَصْبَحْتُ مِنْكَ فِي نِعْمَةٍ وَعَافِيَةٍ وَسِتْر فَأَتِمَّ عَلَيَّ نِعْمَتَكَ وَعَافِيَتَكَ وَسِتْرَكَ فِي الدُّنْيَا وَالآخِرَة",
+    translation: "Ya Allah, sesungguhnya aku telah memasuki waktu pagi dalam keadaan mendapat nikmat, sehat dan dalam keadaan tertutup. Maka sempurnakanlah atas diriku nikmat, kesehatan dan perlindungan-Mu di dunia dan akhirat.",
+    transliteration: "Allāhumma innī ash-baḥtu minka fī ni‘matin wa ‘āfiyatins-sitr fa-atimm ‘alayya ni‘matak wa ‘āfiyatika wa sitraka fīd-dunyā wal-ākhirah",
+    morning: true,
+    evening: false,
+    lite: true,
     repeat: 10
   },
   {
     id: 12,
-    name: "Istighfar",
-    arabic: "أَسْتَغْفِرُ اللَّهَ وَأَتُوبُ إِلَيْهِ",
-    translation: "Aku memohon ampun kepada Allah dan bertaubat kepada-Nya",
-    transliteration: "Astaghfirullāha wa atūbu ilaih.",
+    name: "Dzikir pagi",
+    arabic: "اللَّهُمَّ مَا أَصْبَحَ بِيْ مِنْ نِعْمَةٍ أَوْ بِأَحَدٍ مِنْ خَلْقِكَ فَمِنْكَ وَحْدَكَ لاَ شَرِيْكَ لَكَ فَلَكَ الْحَمْدُ وَلَكَ الشُّكْرُ",
+    translation: "Ya Allah, apa saja nikmat yang aku rasakan pagi ini atau yang diperoleh oleh salah seorang makhluk-Mu, maka semuanya datang dari-Mu semata, tiada sekutu bagi-Mu. Bagi-Mu segala puji dan syukur.",
+    transliteration: "Allāhumma mā ash-baha bī min ni'matin aw bi-ahadin min khalqika faminka waḥdaka lā syarīka laka falaka al-ḥamdu wa laka ash-shukr",
+    morning: true,
+    evening: false,
+    lite: true,
     repeat: 100
   }
 ];
