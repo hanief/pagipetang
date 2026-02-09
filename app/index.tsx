79f37d46d630d0befe638e8cb46f8c0ad1b65e19
Hanief <hanief@gmail.com>
2026-02-09 10:03:38 +0700

    add plausible

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,31 @@
-import { View } from "react-native";
-import Supplication from "./Supplication";
+import { View } from "react-native"
+import Supplication from "./Supplication"
+import Head from "expo-router/head"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Supplication />
-    </View>
-  );
+    <>
+      <Head>
+        <script
+          async
+          src="https://analytics.algoritama.com/js/pa-flim1eufJUCP9hCpcNpY0.js"
+        ></script>
+        <script>
+          window.plausible=window.plausible||function()
+          {(plausible.q = plausible.q || []).push(arguments)}
+          ,plausible.init=plausible.init||function(i){(plausible.o = i || {})};
+          plausible.init()
+        </script>
+      </Head>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Supplication />
+      </View>
+    </>
+  )
 }
